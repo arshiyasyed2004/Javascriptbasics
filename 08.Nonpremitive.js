d80eed289de6663ea9107e08c6e547a46d1a66b0
arshiyasyed2004 <syedarshiyaanjum49@gmail.com>
2025-08-28 11:50:47 +0530

    Create 08.Nonpremitive.js

---
+++ 08.Nonpremitive.js
@@ -0,0 +1,18 @@
+let student={
+    name:`Arshiya`,
+    rollno: 175,
+}
+
+console.log(student); // prints whole object
+console.log(student.name);//prints name : Arshiya
+console.log(student.rollno);//prints rollno: 175
+console.log(typeof(student.rollno)); //prints type of roll number i.e number
+
+let arr=[1,2,3,4];
+console.log(arr); //output: [ 1, 2, 3, 4 ]
+
+//Date datatype
+//You can also print date, like current date using js
+
+const date= new Date(); //used const becuase, date is always going to be constant and same
+console.log(date);
