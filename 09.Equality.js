6006e7f25f358129d93437d641b24e1576bc40f6
arshiyasyed2004 <syedarshiyaanjum49@gmail.com>
2025-08-28 11:53:28 +0530

    Create 09.Equality.js

---
+++ 09.Equality.js
@@ -0,0 +1,15 @@
+//Strict Equality === and Loose Equality
+// Strict equality check if the values and data types both are eqaul eg:-
+let x=10;
+let y= "10";
+if(x==y); console.log("Equal");
+if(x===y); console.log("Not Equal");
+//Always use strict quality
+// O/P
+// Equal
+// Not Equal
+
+//Logical operators:- &&- If all operands are true, it returns true
+//||- OR, if any one operand is true, it returns true
+//!, Opposite to the given output
+//if, switch and loops all work excatly as in java 
