0bbf8eb50a5704c6fce1d98ae6ce4c82e28d3fae
arshiyasyed2004 <syedarshiyaanjum49@gmail.com>
2025-08-28 11:29:12 +0530

    Create 06.OtherVariables.js

---
+++ 06.OtherVariables.js
@@ -0,0 +1,16 @@
+//in Js, we have boolean value true or false
+let x=false;
+console.log(x);
+if(x){
+    console.log("Code is working");
+}
+else{
+    console.log("code is not working")
+}
+ 
+//we can assign values further
+let name = null;
+console.log(name);
+
+name = "Arshiya";
+console.log(typeof(name));//output: String
