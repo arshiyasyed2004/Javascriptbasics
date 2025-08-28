4cec5e88cabf403cf5d2ff74b2111ac8fb7633bb
arshiyasyed2004 <syedarshiyaanjum49@gmail.com>
2025-08-28 11:14:43 +0530

    Create 04.Strings.js

---
+++ 04.Strings.js
@@ -0,0 +1,7 @@
+let stringname = `Arshiyasyed`;
+console.log(stringname);//output:Arshiyasyed name =
+
+let name = "Anjum";
+
+stringname = 'Arshiyasyed ${name}`;
+console.log(stringname); // output: ArshiyasyedAnjum;
