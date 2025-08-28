6a43d8e55de797927da5fc692bd863497df1ffe0
arshiyasyed2004 <syedarshiyaanjum49@gmail.com>
2025-08-28 11:20:37 +0530

    Create 05.Number.js

---
+++ 05.Number.js
@@ -0,0 +1,13 @@
+//in js, we don't have different dataypes like int and float to distinguish between decimals, and whole number
+//Instead we have one datatype, (maybe you guessed it) called numbers for everything
+
+let num1 = 3.55;
+let num2 = 5;
+
+let sum = num1+num2;
+let div = num1/num2;
+let mul = num1*num2;
+console.log(sum);
+console.log(div);
+console.log(mul);
+console.log(typeof(num1)); //output: gives number, in Javascript usually we dont have datatypes, we have only number,string.
