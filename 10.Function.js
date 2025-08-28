f69f4fea0d16c731da78af98607c3065cde118a6
arshiyasyed2004 <syedarshiyaanjum49@gmail.com>
2025-08-28 11:59:02 +0530

    Create 10.Function.js

---
+++ 10.Function.js
@@ -0,0 +1,33 @@
+//Function:- A block of code designed to perform a particular task
+//step 1- Declare a function
+//step 2- call a function, eg
+
+function square(n){
+    return n*n;
+} //--> this is function declaration, whoch declares a function square and it returns the square of the number passed in the parameters
+
+square(8);
+//OP:-
+// 
+// === Code Execution Successful ===
+//why is there no output for this?, it is because we haven't print anything on the console
+let y=square(8);
+console.log(y);
+//O/P
+// 64
+
+// === Code Execution Successful ===
+
+//Function declaration
+function name(params){
+    //executable statements
+}
+
+//Function Expression
+const mane= function (params){
+        //executable statements
+}
+//arrow functions, this is what we use mostly in js
+const aenm= (params) ==>{
+    //executable statements
+}
