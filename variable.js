08c2fd07f4b0f5d95bdaaff2b2f59d0aa128a9db
arshiyasyed2004 <syedarshiyaanjum49@gmail.com>
2025-08-28 11:01:57 +0530

    Create variable.js

---
+++ variable.js
@@ -0,0 +1,19 @@
+/*special characters $,_ are allowed*/
+/*rules
+Cannot use identifiers that JS already has, eg var, const, function
+Should start with lower case letter
+$,_ are allowed, no there specail characters are allowed */
+
+
+let _name = "syed";
+console.log(_name);//output: syed
+
+_name = "Arshiya";
+console.log(_name); //output: Arshiya
+
+const pi = 22/7;
+console.log(pi); //3.14.....
+
+pi = 3.14;
+console.log(pi);//we cant change the value of constant once its assigned
+
